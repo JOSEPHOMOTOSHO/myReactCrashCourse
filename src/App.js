17da40931571b22b202550a081d71102f0e60439
Omotosho Joseph <Joseph.omotosho98@gmail.com>
2021-09-12 04:10:52 +0100

    redo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import Header from "./components/Header";
 
 function App() {
-  return (
-    <div className="container">
-      <Header />
-    </div>
-  );
+  return <div className="container"></div>;
 }
 
 export default App;
