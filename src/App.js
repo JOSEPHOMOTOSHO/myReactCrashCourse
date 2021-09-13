0ca455e960a91571bbddd64f305429d2e7e06a9a
Omotosho Joseph <Joseph.omotosho98@gmail.com>
2021-09-13 05:57:37 +0100

    learnedUseState and created a branch for it.App.js contains what i learnt

--- src/App.js
+++ src/App.js
@@ -1,7 +1,73 @@
-import Header from "./components/Header";
+import { useState } from "react";
+const App = () => {
+  const stories = [
+    {
+      title: "React",
+      url: "https://reactjs.org/",
+      author: "Jordan Walke",
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org/",
+      author: "Dan Abramov, Andrew Clark",
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
 
-function App() {
-  return <div className="container"></div>;
-}
+  const [searchTerm, setSearchTerm] = useState("");
+  return (
+    <div className="container">
+      <Heading />
+      <Label searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      <ListRender list={stories} />
+    </div>
+  );
+};
+
+const ListRender = (props) => {
+  return props.list.map((item) => {
+    <h1>Good</h1>;
+    return (
+      <div key={item.objectID}>
+        <span>
+          <a href={item.url}> {item.title}</a>
+        </span>
+        <span>{item.author}</span>
+        <span>{item.num_comments}</span>
+        <span>{item.points}</span>
+      </div>
+    );
+  });
+};
+
+const Label = ({ searchTerm, setSearchTerm }) => {
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+  return (
+    <>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" onChange={handleChange} />
+
+      <hr />
+      <p>
+        Searching for: <strong>{searchTerm}</strong>
+      </p>
+    </>
+  );
+};
+
+const Heading = () => {
+  return (
+    <>
+      <h1>Omotosho Joseph is learning React</h1>
+    </>
+  );
+};
 
 export default App;
